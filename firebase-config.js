961e5eecbe1dd28d1b770ea48da60baffec1a2d8
Sari Dular <rabilocchanhembram@gmail.com>
2026-08-22 20:28:09 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase Console se apne project ka Web App config yahan paste karein.
-export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
